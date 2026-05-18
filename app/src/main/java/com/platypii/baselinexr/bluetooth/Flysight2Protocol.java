0776111448e83ae8cd2625647703863139d7c410
Hartman Rector <hartmandrector@hotmail.com>
2026-05-17 21:15:08 -0600

    chore: fix scene export config and update Gradle/IDE setup

--- app/src/main/java/com/platypii/baselinexr/bluetooth/Flysight2Protocol.java
+++ app/src/main/java/com/platypii/baselinexr/bluetooth/Flysight2Protocol.java
@@ -46,12 +46,12 @@ public class Flysight2Protocol extends BleProtocol {
 
     @Override
     public boolean canParse(@NonNull BluetoothPeripheral peripheral, @Nullable ScanRecord record) {
-        return peripheral.getName().equals("KFS1975"); // JebFlySight
-//        if (isFlysight(peripheral, record)) {
-//            Log.i(TAG, "Found flysight device: " + peripheral.getName() + " " + peripheral.getAddress());
-//            return true;
-//        }
-//        return false;
+        //return peripheral.getName().equals("FlySightDev"); // HartmanFlySightdev
+        if (isFlysight(peripheral, record)) {
+            Log.i(TAG, "Found flysight device: " + peripheral.getName() + " " + peripheral.getAddress());
+            return true;
+        }
+        return false;
     }
 
     private boolean isFlysight(@NonNull BluetoothPeripheral peripheral, @Nullable ScanRecord record) {
